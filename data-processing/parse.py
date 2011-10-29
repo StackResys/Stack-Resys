77aa3a5322cc931ee707e2af3078971dfc7f1f47
Kai Liu <hfevers@gmail.com>
2011-10-29 15:33:22 -0400

    update the parse.py

--- data-processing/parse.py
+++ data-processing/parse.py
@@ -1,7 +1,9 @@
+from vectorize import Vectorizor
 from xml.sax import ContentHandler, parseString, SAXParseException
-import re
-import pymongo
 import config
+import os
+import pymongo
+import re
 
 def sanitize(content, tag_replaced_by = ""):
     """ Remove the html tags form the content """
@@ -9,7 +11,7 @@ def sanitize(content, tag_replaced_by = ""):
 
 def extract_tags(tag_text):
     """ Extract tags """
-    return tag_text.strip("><").split("><"),
+    return tag_text.strip("><").split("><")
 
 def merge_dict(dict1, dict2):
     """
@@ -23,35 +25,51 @@ def merge_dict(dict1, dict2):
     return dict1
 
 class PostEventHandler(ContentHandler):
-    def __init__(self):
-       ContentHandler.__init__( self )
+    def __init__(self, db_config, override=False):
+       """
+       @param override: erase existing database?
+       """
+       ContentHandler.__init__(self)
        self.tag = "row"
        self.answer_type = "2"
        self.question_type = "1"
-       self.post_count = 0
+       self.vectorizor = Vectorizor(
+        [line[:-1] for line in open(config.INPUT_FILE["stop_words"])])
 
        # Creating the database connection
-       self.connection = pymongo.Connection(
+       self._connection = pymongo.Connection(
                config.DB["server"],
                config.DB["port"])
 
-       database = config.DB["database"]
-       collection = config.DB["collection"]
-       self.questions = self.connection[database][collection]
+       database = db_config["database"]
+       collection = db_config["collection"]
+       self.questions = self._connection[database][collection]
 
-       # TODO: this is just used for test
-       self.questions.remove()
+       if override:
+           self.questions.remove()
 
-       indexes = config.DB["indices"]
+       indexes = db_config["indices"]
        self.questions.create_index(indexes)
 
-    def _add_attr(self, question, attributes, key, parse_by = None):
-        if not attributes.has_key(key):
-            return self
+    # -- Even Handling
+    def startElement(self, name, attributes):
+        assert name == self.tag
 
-        val = attributes.getValue(key)
-        question[key] = val
-        return self
+        if attributes.getValue("PostTypeId") == self.question_type:
+            question = self._create_question(attributes)
+            self.questions.insert(question)
+            vector =  self.vectorizor.vectorize(question["Body"],
+                                                question["Tags"])
+        elif attributes.getValue("PostTypeId") == self.answer_type:
+            answer = self._create_answer(attributes)
+            parent_id = answer["ParentId"]
+            self.questions.update({"Id": parent_id},
+                    {"$push": {"Answers": answer}})
+        else:
+            assert(False)
+
+    def close():
+        self._connection.close()
 
     # -- Parse the Question
     def _create_post(self, attributes):
@@ -75,34 +93,59 @@ class PostEventHandler(ContentHandler):
             ._add_attr(answer, attributes, "CommentCount")
         return answer
 
-    # -- Even Handling
-    def startElement(self, name, attributes):
-        assert name == self.tag
+    def _add_attr(self, question, attributes, key, parse_by = None):
+        if not attributes.has_key(key):
+            return self
 
-        if attributes.getValue("PostTypeId") == self.question_type:
-            question = self._create_question(attributes)
-            self.questions.insert(question)
-        elif attributes.getValue("PostTypeId") == self.answer_type:
-            answer = self._create_answer(attributes)
-            parent = answer["ParentId"]
-            self.questions.update({"id": parent},
-                    {"$push": {"answers": answer}})
-        else:
-            assert(False)
-
-        self.post_count += 1
-        if self.post_count % 100 == 0:
-            print(self.post_count)
+        val = attributes.getValue(key)
+        if parse_by is not None:
+            val = parse_by(val)
+        question[key] = val
+        return self
 
 class ErrorHandler:
     def fatalError(self, msg):
         print(msg)
 
+def dump_stat(dic, path):
+    sorted_items = sorted(dic.items(), key=lambda v: v[1], reverse = True)
+    output = open(path, "w")
+    for item in sorted_items:
+        output.write("%s: %s\n" % item)
+
+def report_progress(i):
+    if i == 0:
+        return
+    if i % 200 == 0:
+        print ".",
+    if i % 2000 == 0:
+        print " -- %d" % i
+
 if __name__ == "__main__":
-    filename = config.INPUT_FILE["file_path"]
-    post_handler = PostEventHandler()
+    filename = config.INPUT_FILE["input"]
+    post_handler = PostEventHandler(config.DB)
     error_handler = ErrorHandler()
 
     with open(filename) as posts_xml:
-        for line in posts_xml:
+        start = config.INPUT_FILE["record_start"]
+        end = config.INPUT_FILE["record_end"]
+        print ("Range: [%d, %d)"% (start, end))
+
+        for i, line in enumerate(posts_xml):
+            if i < start:
+                continue
+            if i >= end:
+                break
+
             parseString(line, post_handler, ErrorHandler())
+            report_progress(i)
+
+    stat_dir = config.INPUT_FILE["stat_dir"]
+    if not os.path.exists(stat_dir):
+        os.makedirs(stat_dir)
+
+    dump_stat(post_handler.vectorizor.tag_count,
+              os.path.join(stat_dir, "tags.stat"))
+    dump_stat(post_handler.vectorizor.word_count,
+              os.path.join(stat_dir, "words.stat"))
+
