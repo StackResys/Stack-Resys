4f5ce4af85971ea3c15c90b8a482b611b8bf6c4c
Kai Liu <hfevers@gmail.com>
2011-12-05 21:56:07 -0500

    move logging code to evaluation directory

---
+++ src/evaluation/log.py
@@ -0,0 +1,31 @@
+""" This module provides a globally accessible
+    logger created from the config file """
+
+import logging
+import os
+
+def _create_logger_from_config():
+    """ Create the logger from the config file """
+    conf = {
+        "name": "StackLogger",
+        "log_file": "logs/experiment.log",
+        "format": "%(asctime)s %(levelname)s \n  >>> %(message)s",
+        "level": logging.DEBUG
+    }
+
+    logging.basicConfig(format=conf["format"])
+    logger = logging.getLogger(conf["name"])
+
+    # if need to write to the log file
+    log_file = conf['log_file']
+    if log_file is not None:
+        handler = logging.FileHandler(conf['log_file'])
+        logger.addHandler(handler)
+
+    logger.setLevel(conf["level"])
+    logger.debug("Logger initialized")
+
+    return logger
+
+LOGGER = _create_logger_from_config()
+
